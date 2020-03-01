2d2340dbea53433fe1c7d186059e0f8911203de8
Alex B <alex.claradelrey@gmail.com>
2020-03-01 20:07:26 +0100

    añadido el archivo IFactura.java

---
+++ facturas/src/facturas/IFactura.java
@@ -0,0 +1,29 @@
+package facturas;
+
+public class IFactura {
+	
+	
+	//objeto estado
+	
+	public IFactura()
+	{
+		
+	}
+	
+	public void cambiarEstado(Estados estado)
+	{
+		
+	}
+	
+	public float getTotal()
+	{
+		
+		return 0;
+	}
+	
+	public float getCantidad()
+	{
+		
+		return 0;
+	}
+}
